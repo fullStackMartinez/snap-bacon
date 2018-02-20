b33ff2bcd5a53b9103b673d09c34d8b03eefc322
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-20 10:06:50 -0700

    bacon snap challenge using JQuery and an api that calls paragraphs from an external site

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+	$("#trigger").click(function()
+	{
+		$.getJSON('https://baconipsum.com/api/?callback=?',
+			{ 'type':'all-meat', 'start-with-lorem':'1', 'paras':'6' },
+			function(baconGoodness)
+			{
+				if (baconGoodness && baconGoodness.length > 0)
+				{
+					$("#baconIpsumOutput").html('');
+					for (var i = 0; i < baconGoodness.length; i++)
+						$("#baconIpsumOutput").append('<p>' + baconGoodness[i] + '</p>');
+					$("#baconIpsumOutput").show();
+				}
+			});
+	});
+});
